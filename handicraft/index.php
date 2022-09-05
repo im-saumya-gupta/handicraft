b94a7954611f586e53049940e95bdc157890e315
Saumya Gupta <63520832+im-saumya-gupta@users.noreply.github.com>
2022-09-06 01:57:19 +0530

    Add files via upload

---
+++ handicraft/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("header.php"); ?>
+</head>
+<body>
+    <?php //include("arrival.html"); ?> 
+    <?php include("choose.html"); ?>
+    <?php //include("expert.html"); ?>
+    <?php //include("customer.html"); ?>
+    <?php //include("blog.html"); ?>
+    <?php //include("client.html"); ?>
+    <?php //include("footer.html"); ?>
+
+</body>
+</html>
